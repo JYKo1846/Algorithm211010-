68fa4675cd77404b3cfed9cb0739598ef656d69e
JYKO <184659@gmail.com>
2021-11-15 18:02:00 +0900

    2021-10-12부터 시작

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <stdio.h>
 
-void makeQ();	//10845 20211011
+void makeQ();	// 10845 20211011
 void makeQ2(); 	// 위 문제를 정적배열로 해결
 void makeQ3(); 	// c++ STL 사용 
+void simpleKnapsack(); // 12865	20211110
+void DFSnBFS();	// 1260 20211110
+void searchMaze(); // 2178 20211115
 
 int main(int argc, char** argv) {
 	//makeQ();
 	//makeQ2();
 	//makeQ3();
+	//simpleKnapsack();
+	//DFSnBFS();
+	//searchMaze();
 	return 0;
 }
