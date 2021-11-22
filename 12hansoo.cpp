ec5600f8b48f471092ee20567ee5acd5cf285572
JYKO <184659@gmail.com>
2021-11-22 21:35:05 +0900

    BOJ 1065 2021.11.22 구현. Brute Force implementation

---
+++ 12hansoo.cpp
@@ -0,0 +1,30 @@
+// BOJ 1065 Hansoo 2021.11.22
+// ez
+
+#include <iostream>
+
+using namespace std;
+
+void hansoo() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	
+	int N;
+	int ans= 0;
+	
+	cin >> N;
+	
+	for(int i= 1; i<= N; i++)
+	{
+		if(i< 100) ans++;
+		else
+		{
+			int fir= i/100;
+			int sec= (i%100)/ 10;
+			int thr= i%10;
+			
+			if((fir-sec) == (sec-thr)) ans++;
+		}
+	}
+	cout << ans << "\n";
+}
