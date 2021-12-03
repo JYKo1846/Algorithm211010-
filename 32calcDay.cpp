4febe271e821227d2a612424686f0643903bae0f
JYKO <184659@gmail.com>
2021-12-03 13:08:02 +0900

    BOJ 1476 날짜 계산. 2021.12.03완료. 간단한 Bruteforce 알고리즘. 15*i+e로 나오는 값중 태양해와 달해값을 만족하는값을 찾으면 된다

---
+++ 32calcDay.cpp
@@ -0,0 +1,23 @@
+//BOJ 1476 Calculation day 2021.12.03
+// Use BruteForce. Simple Bruteforce problem. calculate LCM ( Least Common Multiple 0
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void calcDay() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int E, S, M;
+	int ans;
+	
+	cin >> E >> S >> M;
+	for(int i= 0; i< 8000; i++) {
+		ans= (15*i)+ E;
+		if((ans-S)%28== 0)
+			if((ans-M)%19== 0)
+				break;
+	}
+	cout << ans << "\n";
+}*/
