e74b4e2c160b97ace02570a736c19735a2bcc485
JYKO <184659@gmail.com>
2021-12-04 23:47:17 +0900

    BOJ 15686 치킨배달 2021.12.04부터 시작

---
+++ 33chickenDeli.cpp
@@ -0,0 +1,46 @@
+//BOJ 15686 Chicken Delivery 2021.12.04
+/*
+#include <bits/stdc++.h>
+
+struct pos{
+	int x;
+	int y;
+}typedef pos;
+
+using namespace std;
+
+int maps[53][53];
+int dist[15][105];		// dist[chicken store number][house number] = store ~ house distance
+pos house[105];			// house's coordinate
+
+void chickenDeli() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	int N, M;
+	int i, j;
+	int stN= 0, houseN= 0;	// stN : store number, houseN : house number
+	
+	cin >> N >> M;
+	
+	for(i= 1; i<= N i++) 
+	{
+		for(j= 1; j<= N; j++)
+		{
+			cin >> maps[i][j];
+			if(maps[i][j]== 2) 
+			{
+				store[stN].x= i;
+				store[stN++].y= j;
+			}
+			
+			if(maps[i][j]== 1) 
+			{
+				house[houseN].x= i;
+				house[houseN++].y= j;
+			}
+		}
+	}
+	
+	
+*/	
