c6009f8523d9dd52f3cb66f4ec6a8cf0069b0ca7
JYKO <184659@gmail.com>
2021-12-05 17:00:35 +0900

    BOJ 15686 치킨배달. 2021.12.05완료. 그래프화 & DFS사용한 BF!! ㅇㄹㄴ 사용시 항상 상태 저장을 잊지 말것!

--- 33chickenDeli.cpp
+++ 33chickenDeli.cpp
@@ -1,6 +1,10 @@
-//BOJ 15686 Chicken Delivery 2021.12.04
+// BOJ 15686 Chicken Delivery 2021.12.04
+// use DFS
+// IDEA was corrected. But, in dfs(), curD[j] = min(...) => tmpD[j]. In dfs, save status, and use temporary status.
+// But I was forgotten. KEEP IN MIND!!!
 /*
 #include <bits/stdc++.h>
+#include <math.h>
 
 struct pos{
 	int x;
@@ -9,38 +13,72 @@ struct pos{
 
 using namespace std;
 
-int maps[53][53];
-int dist[15][105];		// dist[chicken store number][house number] = store ~ house distance
-pos house[105];			// house's coordinate
+int stN, hseN;			// stN : store number, houseN : house number
+int ans;
+int N, M;
+int dist[105];			// dist[house number] = store ~ "NUMBER" house distance
+bool remain[15];		// during DFS, store, will remain
+pos hse[105];			// house's coordinate
+pos st[15];				// store's coordinate
+
+
+void dfs(int cur, int curStN, int *curD) {				// cur: current store need calculate. curStN: current remained store number.
+	int i, j;											// curD: current store ~ house's minimum distance.
+	
+	if(curStN == M)
+	{
+		int t= 0;
+		for(i= 0; i< hseN; i++) t+= curD[i];
+	 	ans= min(t, ans);
+	 	return ;
+	 }
+	
+	int tmpD[hseN+1];
+	
+	for(i= cur+1; i< stN; i++)
+	{
+		for(j= 0; j< hseN; j++) tmpD[j]= curD[j];
+		for(j= 0; j< hseN; j++)
+			tmpD[j]= min(tmpD[j], abs(st[i].x- hse[j].x)+ abs(st[i].y- hse[j].y));
+		dfs(i, curStN+1, tmpD);
+	}
+}
 
 void chickenDeli() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	
-	int N, M;
 	int i, j;
-	int stN= 0, houseN= 0;	// stN : store number, houseN : house number
+	ans= 10000;
+	stN= 0, hseN= 0;	// stN : store number, houseN : house number
 	
 	cin >> N >> M;
-	
-	for(i= 1; i<= N i++) 
+	for(i= 0; i< N; i++) 
 	{
-		for(j= 1; j<= N; j++)
+		for(j= 0; j< N; j++)
 		{
-			cin >> maps[i][j];
-			if(maps[i][j]== 2) 
+			int t;
+			if(t== 2) 
 			{
-				store[stN].x= i;
-				store[stN++].y= j;
+				st[stN].x= i;
+				st[stN++].y= j;
 			}
 			
-			if(maps[i][j]== 1) 
+			if(t== 1) 
 			{
-				house[houseN].x= i;
-				house[houseN++].y= j;
+				hse[hseN].x= i;
+				hse[hseN++].y= j;
 			}
 		}
+	}	
+	
+	for(i= 0; i< stN; i++)
+	{
+		for(j= 0; j< hseN; j++) dist[j]= 120;
+		for(j= 0; j< hseN; j++)
+			dist[j]= min(dist[j], abs(st[i].x- hse[j].x) + abs(st[i].y- hse[j].y));
+		dfs(i, 1, dist);
 	}
 	
-	
-*/	
+	cout << ans << "\n";
+}*/
