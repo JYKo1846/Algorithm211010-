defae1e3e740dc2a312e7d949f6088bc439f8c49
JYKO <184659@gmail.com>
2021-12-17 23:31:07 +0900

    BOJ 15684 사다리 조작. 2021.12.17 구현중. 내일 탐색함수 다시 구현

--- 52ladderControl.cpp
+++ 52ladderControl.cpp
@@ -1,6 +1,6 @@
 // BOJ 15684 ladder control 2021.12.16
 // First, imply ladder operation
-
+/*
 #include <iostream>
 #include <cstring>
 #include <vector>
@@ -13,7 +13,7 @@ struct ver{
 }typedef ver;
 
 vector<ver> lad[32];		// lad[i] : data of " horizon a ---- < data > ---- horizon b "
-bool isLine[12][12];		// isLine[i][j] : i---j line existed, 1. else, 0
+bool isLine[15][15];		// isLine[i][j] : i---j line existed, 1. else, 0
 int N, M, H;
 int ans= 199;				// answer that minimum line number that mv()== true
 
@@ -47,7 +47,9 @@ bool mv() {					// find 1~ N's destination function, If one of line i don't reac
 	}
 	return true;
 }
-		
+*/
+// imply wrong way. need new function
+/*		
 void dfs(int curH, int m) {		// curH : current height.  m : current line number made
 	if(m> 3)
 		return ;
@@ -61,21 +63,22 @@ void dfs(int curH, int m) {		// curH : current height.  m : current line number 
 	ver tv;
 	for(int i= curH+1; i<= H; i++)
 	{
-		for(int j= 2; j<= N-1; j++)
+		for(int j= 1; j<= N-1; j++)
 		{
 			
 			int sz= lad[curH].size()-1;
 			
 			// create ladder to left of line
-			if(!isLine[j][j-1] &&
+			if((j> 1 && !isLine[j][j-1]) &&
 				(j+1 <= N) &&
 				!isLine[j][j+1])
 			{
+				cout << " IN CREATE LEFT LADDER" << "\n";
 				isLine[j][j-1]= 1;
 				tv.st= j;	tv.dst= j-1;
-				lad[curH].push_back(tv);
-				dfs(curH+1, m+1);
-				lad[curH].erase(lad[curH].begin()+ sz+1);
+				lad[i].push_back(tv);
+				dfs(i, m+1);
+				lad[i].erase(lad[i].begin()+ sz+1);
 			}
 			
 			// create ladder to right of line
@@ -83,16 +86,18 @@ void dfs(int curH, int m) {		// curH : current height.  m : current line number 
 				!isLine[j][j-1] &&
 				((j==N-1) || (!isLine[j+1][j+2])) )
 			{
+				cout << " IN CREATE RIGHT LADDER" << "\n";
 				isLine[j][j+1]= 1;
 				tv.st= j; tv.dst= j+1;
-				lad[curH].push_back(tv);
-				dfs(curH+1, m+1);
-				lad[curH].erase(lad[curH].begin()+ sz+1);
+				lad[i].push_back(tv);
+				dfs(i, m+1);
+				lad[i].erase(lad[i].begin()+ sz+1);
 			}
 		}
 	}
 } 
-
+*/			 
+/*
 void ladderControl() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
@@ -119,7 +124,14 @@ void ladderControl() {
 		lad[tlv].push_back(tl);
 	}
 
+	for(i= 1; i<= N; i++)
+	{
+		for(j= 1; j<= N; j++) cout << isLine[i][j] << " ";
+		cout << "\n";
+	}
+
 	dfs(0, 0);
+
 	if(ans== 199) ans= -1;
 	cout << ans << "\n";
-}
+}*/
