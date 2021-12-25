12863e8aea68bc6f6e6a83afd22d8e0b0dc59a0f
JYKO <184659@gmail.com>
2021-12-25 22:05:58 +0900

    BOJ 1051 숫자사각형. 단순한 BF. 사각형의 최대길이는 보드의 가로,세로 중 짧은 길이

---
+++ 65numSquare.cpp
@@ -0,0 +1,46 @@
+// BOJ 1051 number square 2021.12.25
+/*
+#include <iostream>
+
+using namespace std;
+
+int bd[52][52];		// board
+int N, M;			// N: row, M: column
+int maxL;			// maxL: square's maximum available length
+
+void numSquare() {
+	int i, j;
+	int ans= 1;
+	
+	scanf("%d %d", &N, &M);
+	
+	for(i= 0; i< N; i++)
+		for(j= 0; j< M; j++)
+			scanf("%1d", &bd[i][j]);
+			
+	maxL= N < M ? N : M;
+	
+	for(i= 0; i< N; i++)
+	{
+		for(j= 0; j< M; j++)
+		{
+			int st= bd[i][j];
+			
+			for(int l= 1; l< maxL; l++)
+			{
+				int nx= i+ l;
+				int ny= j+ l;
+				if((nx< N) && (ny< M))
+				{
+					if((st== bd[nx][j]) && (st== bd[i][ny]) && (st== bd[nx][ny]))
+					{
+						int sz= (l+1)*(l+1);
+						ans= ans > sz ? ans : sz;
+					}
+				}
+			}
+		}
+	}
+	
+	printf("%d\n", ans);
+}*/
