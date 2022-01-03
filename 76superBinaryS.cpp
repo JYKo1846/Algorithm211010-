7bcf7b0f67728f4a7117ddd28320984fe2482522
JYKO <184659@gmail.com>
2022-01-03 23:03:43 +0900

    BOJ 15641 슈퍼슈퍼바이너리탐색. 2022.01.03완료. BF.

---
+++ 76superBinaryS.cpp
@@ -0,0 +1,14 @@
+// BOJ 15641 super super binary search 2022.01.03
+// useless problem
+/*
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+*/
+/*
+void superBinaryS() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+}*/
