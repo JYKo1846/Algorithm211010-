7d6f5e2e3b790daf29cb33777349d0e91b83e4f2
JYKO <184659@gmail.com>
2022-03-07 22:26:25 +0900

    BOJ 17825 주사위 윷놀이. 2022.03.07완료. dfs를 통해 10개의 턴 중 움직일 말들의 경우의 수를 정해, 계산. 이때, 백트래킹을 이용해 유효하지 않은 경우, return해 성공. 칸을 잘못 세고, 40 칸을 수평으로 도달할때, 대각선으로 도달 할 때의 중복 경우를 생각하지 않아 틀렸었음. 그리고, 수행시간이 파이썬 만큼 걸림. 경우의 수를 모두 따지는 곳에서 시간이 걸리는것으로 생각. 하지만, 어떻게 수정해야될지는 감이 안잡힘 나중에 복습때 다시 생각하기

--- 111diceYut.cpp
+++ 111diceYut.cpp
@@ -1,4 +1,11 @@
 // BOJ 17825 diceYut 2022.02.26
+// 2022.03.07 completed!!!!!
+// Why I was wrong : 1) miscalculate blank
+//					 2) lenient conditional sentence. => ex)) if(20< bd[] && bd< 25) 
+//					 3) bd[20] && bd[32] was same place. But, I wasn't thinking straight
+// But, Finally finished. !!!!!!
+// BUT, Running time is same as python running time. maybe, choose moving token function dfs() was problem.
+// Repeat time, Modify dfs(). ERASE duplicated case ex) 0100.... & 0200.... 
 /*
 #include <iostream>
 #include <cstring>
@@ -8,6 +15,7 @@ using namespace std;
 int mv[11];							// availiable move square number at each turn
 int tkn[5];							// each token's current square. tkn[x]== 45 : reached token
 int mvTkn[11];						// moved token at each turn
+int compareTkn[11];					
 int bd[35]= {0, 2, 4, 6, 8, 10,
 				12, 14, 16, 18, 20,
 				22, 24, 26, 28, 30,
@@ -22,21 +30,30 @@ int ans= -1;
 bool valid() {
 	int i, j;
 	for(i= 0; i< 4; i++)
-		for(j= i+1; j< 5; j++)
-			if(tkn[i]== tkn[j]) 
-				if(tkn[i]!= 45) return false;
+	{
+		for(j= i+1; j< 4; j++)
+		{
+			if(tkn[i]== tkn[j] || ((tkn[i]==20 || tkn[i]== 32) && (tkn[j]== 20 || tkn[j]== 32)))
+			{ 
+				if(tkn[i]!= 45)
+				{
+					return false;
+				}
+			}
+		}
+	}
 	return true;
-}
+}   
 
 void calc() {
 	int i;
-	for(i= 0; i< 10; i++) cout << mvTkn[i] << " ";
-	cout << "\n";
+	//for(i= 0; i< 10; i++) cout << mvTkn[i] << " ";
+	//cout << "\n";
 	
 	int cur= -1;
 	int st= 0, ntmv= 0;			// token's start position, next move
 	int tmpR= 0;
-	for(i= 0; i< 5; i++) tkn[i]= (i*-1) -1;
+	for(i= 0; i< 4; i++) tkn[i]= (i*-1) -1;
 	
 	for(int t= 0; t< 10; t++)
 	{
@@ -59,21 +76,25 @@ void calc() {
 			else tkn[cur]= 29+ (ntmv- 3);
 		}
 		else if(st== 15)
-			tkn[cur]= 25+ ntmv;
-		else if(20< st && st< 29)
+		{
+			if(ntmv< 4) tkn[cur]= 25+ ntmv;
+			else tkn[cur]= 29+ (ntmv- 4);
+		}
+		else if(20< st && st< 31)
 		{
 			tkn[cur]+= ntmv;
+			
 			if(20< st && st< 24)
 			{
-				if(tkn[cur]> 23) tkn[cur]= 29+ (tkn[cur]- 23);
+				if(tkn[cur]> 23) tkn[cur]= 29+ (tkn[cur]- 24);
 			}
 			else if(23< st && st< 26)
 			{
-				tkn[cur]= 29+ (tkn[cur]- 25);
+				if(tkn[cur]> 25) tkn[cur]= 29+ (tkn[cur]- 26);
 			}
 			else if(25< st && st< 29)
 			{
-				tkn[cur]= 29+ (tkn[cur]- 28);
+				if(tkn[cur]> 28) tkn[cur]= 29+ (tkn[cur]- 29);
 			}
 		}
 		else
@@ -81,27 +102,36 @@ void calc() {
 		
 		// calculate operation
 		if(tkn[cur]> 20)
-			if(15< st && st< 21)
+			if((15< st && st< 21) || (st> 20 && tkn[cur]> 32))
 				tkn[cur]= 45;
+			else
+				tmpR+= bd[tkn[cur]];
 		else
 			tmpR+= bd[tkn[cur]];
+			
 	}
 	
-	ans= ans> tmpR ? ans: tmpR;
-	cout << "TEMP RESULT : " << tmpR << endl;
+	if(!valid()) return ;
+	
+	if(ans< tmpR)
+	{
+		ans= tmpR;
+		for(i= 0; i< 10; i++) compareTkn[i]= mvTkn[i];
+	}
+	//cout << "TEMP RESULT : " << tmpR << endl;
 }
 
-void dfs(int cT, int tkN) {
+void dfs(int cT) {
 	if(cT== 10) 
 	{
 		calc();
 		return ;
 	}
-	
-	for(int i= tkN; i< 4; i++)
+	 
+	for(int i= 0; i< 4; i++)
 	{
 		mvTkn[cT]= i;
-		dfs(cT+1, i);
+		dfs(cT+1);
 	}
 }
 
@@ -115,6 +145,10 @@ void diceYut() {
 	
 	for(i= 0; i< 10; i++) cin >> mv[i];
 	
-	dfs(0, 0);
-	cout << ans << endl;
+	dfs(0);
+	//for(i= 0; i< 10; i++) mvTkn[i]= 0;
+	//calc();
+	cout << ans << "\n";
+	for(i= 0; i< 10; i++) cout << compareTkn[i] << " ";
+	cout << "\n";
 }*/
