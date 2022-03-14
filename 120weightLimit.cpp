461b3aa0e05044f013cb28416dde0ce4096bbe1e
JYKO <184659@gmail.com>
2022-03-14 23:00:29 +0900

    BOJ 1939 무게제한. 2022.03.14 시작. BFS로 구현했으나 생각하지 못했던 케이스들이 있었다. 이 문제는 가장 빨리 도착하는것이 아닌, 최대한 무게를 보존하며 진행해야 된다. DFS로 각 탐색에 대해서 방문여부를 체크하는것으로 내일 다시 구현하기

---
+++ 120weightLimit.cpp
@@ -0,0 +1,23 @@
+// BOJ 1939 weight limit 2022.03.14
+// IMPLY WITH DFS. VISIT CHECK TO EVERY LOOP
+/*
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> v[10005];
+bool vst[10005];
+int st, dst;
+int N, M;
+
+void weightLimit() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int t1, t2, wt, i;
+	
+	cin >> N >> M;
+	for(i= 0; i< M; i++)
+	{
+		cin >> t1 >> t2 >> wt;
+		*/
